1805a9193a43de18ee3929413344d3ef33211262
ssinthy <sumaiya.sultana.sinthy@gmail.com>
2024-08-06 12:02:53 +0200

    disable manual controller

--- CARLA_client/main.py
+++ CARLA_client/main.py
@@ -10,11 +10,11 @@ def main():
 
     threading.Thread(target=monitor_odd, args=[ego_vehicle, emergency_vehicle, world]).start()
     # threading.Thread(target=set_spectator, args=[world, ego_vehicle]).start()
-    manual_control(world, ego_vehicle, sensor_data)
+    # manual_control(world, ego_vehicle, sensor_data)
 
     # Cleanup
-    camera.stop()
-    ego_vehicle.destroy()
+    # camera.stop()
+    # ego_vehicle.destroy()
 
 if __name__ == '__main__':
     main()
